193a6ef62b7609bd52fa4fb9723d0e22a0448e4e
Akshdeep <akshd4114@gmail.com>
2026-01-11 18:04:53 +0530

    update welcome message

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello from GitOps 🚀");
+  res.send("Hello from the COMPLETE GitOps Pipeline! 🎉");
 });
 
 app.listen(3000, () => console.log("Running on 3000"));
